67a92b73b6c63d064171139a363d8eab945952c7
Silas Stulz <sst@studer-raimann.ch>
2017-10-04 14:20:32 +0200

    navigation php

---
+++ pages/navigation.php
@@ -0,0 +1,5 @@
+<ul>
+<a  href="index.php"><li class="col-1"> Cars & Cars </li> </a>
+<a  href="products.php"> <li class="col-2"> Products </li> </a>
+<a  href="account.php"> <li class="col-3"> Account </li> </a>
+</ul>
