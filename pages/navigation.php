f57eb79dd151a30ae39515ab4f18025351772589
Phantomanu <32430814+Phantomanu@users.noreply.github.com>
2017-10-05 20:52:56 +0200

    fail

--- pages/navigation.php
+++ pages/navigation.php
@@ -1,5 +1,5 @@
 <ul>
-<a  href="index.php"><li> Cars & Cars </li> </a>
-<a  href="products.php"> <li> Products </li> </a>
-<a  href="account.php"> <li> Account </li> </a>
+<a class="col-2" href="index.php"><li> Cars & Cars </li> </a>
+<a class="col-2" href="products.php"> <li> Products </li> </a>
+<a class="col-2" href="account.php"> <li> Account </li> </a>
 </ul>
