6ef4e884e340b9d052bcf371265b969671ff1308
Phantomanu <32430814+Phantomanu@users.noreply.github.com>
2017-10-22 16:46:43 +0200

    added logo, failed the rest

--- pages/navigation.php
+++ pages/navigation.php
@@ -1,4 +1,7 @@
 <ul>
+<a href="index.php">
+  <img src="../src/logo.png" alt="Webshop logo" style="width:120px;height:73px">
+</a>
 <a class="col-2" href="index.php"><li> Cars & Cars </li> </a>
 <a class="col-2" href="products.php"> <li> Products </li> </a>
 <a class="col-2" href="account.php"> <li> Account </li> </a>
